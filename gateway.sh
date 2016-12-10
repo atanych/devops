7585839fd22b22ded1401099c617e122842afa20
atanych <atanych@gmail.com>
2016-12-11 00:46:43 +0300

    Update gateway.sh

--- gateway.sh
+++ gateway.sh
@@ -56,13 +56,13 @@ server {
     root /usr/share/nginx/www;
   }
  
-	location ~ \.php$ {
-		include fastcgi_params;
-		fastcgi_param APP_ENV prod;
+  location ~ \.php$ {
+    include fastcgi_params;
+    fastcgi_param APP_ENV prod;
 
-		try_files $uri =404;
-		fastcgi_pass unix:/var/run/php5-fpm.sock;
-		fastcgi_index index.php;
-		fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-	} 
+    try_files $uri =404;
+    fastcgi_pass unix:/var/run/php5-fpm.sock;
+    fastcgi_index index.php;
+    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+  } 
 }
