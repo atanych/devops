bcca693049e618025dc16eb4b5447705f30dcae8
atanych <atanych@gmail.com>
2017-01-15 22:37:08 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -8,6 +8,7 @@ docker run -p 8888:80 prakhar1989/static-site # run and map ports
 docker run -d -P --name st static # all ports, daemon, st - container name, static - image name
 docker run -v /var/www:/usr/share/ -d -p 8888:80 --name st static # mount /var/www -> /usr/share
 docker stop <container_name>
+docker start <container_name>
 
 docker rm <container_id> 
 docker rm $(docker ps -a -q -f status=exited) # remove all unused containers
@@ -18,4 +19,8 @@ docker port <container> # opened ports
 docker build -t atanych . # create image from dockerfile
 
 docker exec -it 1da24e7e5f6f bash # enter to container 
+
+docker commit 7c8d922dc0ea atanych/python # commit current container 7c8d922dc0ea to image atanych/python
+docker push atanych/python
+docker login
 
